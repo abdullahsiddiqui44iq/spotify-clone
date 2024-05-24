c019a34c083ae246e5f3cead0bd2ade56363e9f3
102693146 <abdullahsiddiqui44iq@users.noreply.github.com>
2024-05-24 23:01:14 +0500

    chore: Add react-router-dom dependency and update main.jsx and package.json

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,12 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
+import Display from './components/Display';
 
 function App() {
   return (
     <div className='h-screen bg-black'>
       <div className="h-[90%] flex">
         <Sidebar />
+        <Display/>
       </div>
       <Player />
     </div>
--- spotify-clone/src/components/Sidebar.jsx
+++ spotify-clone/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import {assets} from "../assets/assets"
 
 function Sidebar() {
   return (
-    <div className='w-[25%] h-full flex-col gap-2 text-white hidden lg:flex'>
+    <div className='w-[25%] h-full flex-col gap-2 text-white hidden lg:flex m-2'>
         <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
             <div className="flex items-center gap-3 pl-8 cursor-pointer">
                 <img className="w-6" src={assets.home_icon} alt="" />
@@ -15,12 +16,12 @@ function Sidebar() {
             </div>
         </div>
         <div className='bg-[#121212] h-[85%] rounded '>
-            <div className='p-4 flex items center justify-between bg-blue-800'>
-                <div className='flex items-center gap-3 bg-green-600'>
+            <div className='p-4 flex items center justify-between '>
+                <div className='flex items-center gap-3'>
                     <img className='w-8' src={assets.stack_icon} alt="" />
                     <p className='font-semibold'>Your Library</p>
                 </div>
-                <div className='bg-red-600 flex items-center gap-3'>
+                <div className='flex items-center gap-3'>
                     <img className='w-5' src={assets.arrow_icon} alt="" />
                     <img className='w-5' src={assets.plus_icon} alt="" />
 
@@ -28,12 +29,12 @@ function Sidebar() {
             </div>
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                 <h1>Create Your First Playlist</h1>
-                <p className='font-light'>It's easy we will help you</p>
+                <p className='font-light'>It&apos;s easy we will help you</p>
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
             </div>
 
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
-                <h1>Let's find some podcast to follow</h1>
+                <h1>Let&apos;s find some podcast to follow</h1>
                 <p className='font-light'>We will keep you updated on new episodes</p>
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
             </div>
