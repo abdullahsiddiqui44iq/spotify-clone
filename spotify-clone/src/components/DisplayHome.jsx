00c3ee305868ab29652eba0e409fd20fb8c42782
102693146 <abdullahsiddiqui44iq@users.noreply.github.com>
2024-06-01 14:56:04 +0500

    chore: Update DisplayHome component to include featured charts and today's biggest hits

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -1,12 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import Navbar from '../components/Navbar';
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import { albumsData } from "../assets/assets";
+import { songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 function DisplayHome() {
   return (
-   <>
-   <Navbar/>
-   </>
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggets hits</h1>
+        <div className="flex overflow-auto">
+          {}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ spotify-clone/src/components/SongItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SongItem() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default SongItem;
